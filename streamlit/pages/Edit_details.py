78063afb3aaac3331fcd956ee3ed8dfc8cc62880
Harshit Mittal <hmittal15@gmail.com>
2023-04-27 19:41:06 -0400

    Bug fixes

--- streamlit/pages/Edit_details.py
+++ streamlit/pages/Edit_details.py
@@ -1,61 +1,67 @@
-
 import streamlit as st
 import sqlite3
 import requests
 
-BASE_URL = 'http://localhost:8090'
+if 'access_token' not in st.session_state:
+    st.session_state.access_token = ''
 
+def edit_page(my_token):
+    BASE_URL = 'http://localhost:8090'
 
-tab1, tab2 = st.tabs(["Forgot Password", "Change Plan"])
+    tab1, tab2 = st.tabs(["Forgot Password", "Change Plan"])
 
-with tab1:
-    st.header("Change Password")
-    # Create input fields for username and new password
-    username = st.text_input("Username")
-    password = st.text_input("New Password", type="password")
+    with tab1:
+        st.header("Change Password")
+        # Create input fields for username and new password
+        username = st.session_state.username
+        password = st.text_input("New Password", type="password")
 
-    # Create button to update password
-    if st.button("Update Password"):
-        # Check if username and password fields are not empty
-        if username and password:
+        # Create button to update password
+        if st.button("Update Password"):
+            # Check if username and password fields are not empty
+            if username and password:
 
-            # Make a request to the endpoint to check if the username already exists
-            username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
-            status = username_response["user"]
+                # Make a request to the endpoint to check if the username already exists
+                username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
+                status = username_response["user"]
 
-            if status:
-                st.text(f"User {username} not found in database")
-                
+                if status:
+                    st.text(f"User {username} not found in database")
+                    
+                else:
+
+                    # Call function to update password in database
+                    requests.post(BASE_URL + f'/update-password?username={username}&password={password}')
+                    st.success("Password updated successfully!")
             else:
-
-                # Call function to update password in database
-                requests.post(BASE_URL + f'/update-password?username={username}&password={password}')
-                st.success("Password updated successfully!")
-        else:
-            st.error("Please enter both username and new password")
-   
-
-with tab2:
+                st.error("Please enter both username and new password")
     
-    st.header("Change Plan")
 
-    # Get the username and new plan from the user
-    username = st.text_input('Enter username')
-    new_plan = st.selectbox('Select new plan', ['free', 'gold', 'platinum'])
-    
-    if st.button('Upgrade Plan'):
-        if username and new_plan:
+    with tab2:
+        
+        st.header("Change Plan")
 
-            # Make a request to the endpoint to check if the username already exists
-            username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
-            status = username_response["user"]
+        # Get the username and new plan from the user
+        username = st.session_state.username
+        new_plan = st.selectbox('Select new plan', ['free', 'gold', 'platinum'])
+        
+        if st.button('Upgrade Plan'):
+            if username and new_plan:
 
-            if status:
-                st.text(f"User {username} not found in database")
-                
+                # Make a request to the endpoint to check if the username already exists
+                username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
+                status = username_response["user"]
+
+                if status:
+                    st.text(f"User {username} not found in database")
+                    
+                else:
+                    requests.post(BASE_URL + f'/update-plan?username={username}&new_plan={new_plan}')
+                    st.text(f"{username}'s plan has been updated to {new_plan}")
             else:
-                requests.post(BASE_URL + f'/update-plan?username={username}&new_plan={new_plan}')
-                st.text(f"{username}'s plan has been updated to {new_plan}")
-        else:
-            st.error("Please enter both username and new plan")
+                st.error("Please enter both username and new plan")
           
+if "access_token" not in st.session_state or st.session_state['access_token']=='':
+    st.title("Please sign-in to access this feature!")
+else:
+    edit_page(st.session_state["access_token"])
