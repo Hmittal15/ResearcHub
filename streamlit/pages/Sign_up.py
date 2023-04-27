3b8adc55de66ebdc8ebb1a72395c62d776643e40
Harshit Mittal <hmittal15@gmail.com>
2023-04-27 02:40:40 -0400

    Logging and rate-limiting imlemented

--- streamlit/pages/Sign_up.py
+++ streamlit/pages/Sign_up.py
@@ -2,38 +2,40 @@
 import streamlit as st
 import requests
 
+def signup():
+    user_bucket_name = 'researchub'
+    subscription_tiers = ['free', 'gold', 'platinum']
+    BASE_URL = 'http://localhost:8090'
 
+    # Streamlit app
+    st.title('Sign-up Page')
 
-user_bucket_name = 'researchub'
-subscription_tiers = ['free', 'gold', 'platinum']
-BASE_URL = 'http://localhost:8090'
+    # Collect user details
 
+    username = st.text_input('Username')
+    email = st.text_input('Email')
+    fullname = st.text_input('Full Name')
+    password = st.text_input('Password', type='password')
+    plan = st.selectbox('Plan Type', ['free', 'gold', 'platinum'])
+    role = st.selectbox('Role', ['Student', 'Working individual', 'Researcher', 'Others'])
 
-# Streamlit app
-st.title('Sign-up Page')
+    # Handle form submission
+    if st.button('Sign up'):
+        # Make a request to the endpoint to check if the username already exists
+        username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
+        status = username_response["user"]
 
-# Collect user details
+        if status:
+            # Create user
+            requests.post(BASE_URL + f'/add-user?username={username}&password={password}&email={email}&full_name={fullname}&plan={plan}&role={role}')
 
-username = st.text_input('Username')
-email = st.text_input('Email')
-fullname = st.text_input('Full Name')
-password = st.text_input('Password', type='password')
-plan = st.selectbox('Plan Type', ['free', 'gold', 'platinum'])
-role = st.selectbox('Role', ['Student', 'Working individual', 'Researcher', 'Others'])
+            st.success(f"User : {username} created successfully with Name : {fullname} and Subscription Plan : {plan}.")
 
-# Handle form submission
-if st.button('Sign up'):
-    # Make a request to the endpoint to check if the username already exists
-    username_response = (requests.post(BASE_URL + f'/check-user-exists?username={username}')).json()
-    status = username_response["user"]
+        else:
+            st.write("Username already exists.")
+            # raise typer.Abort()
 
-    if status:
-        # Create user
-        requests.post(BASE_URL + f'/add-user?username={username}&password={password}&email={email}&full_name={fullname}&plan={plan}&role={role}')
-
-        st.success(f"User : {username} created successfully with Name : {fullname} and Subscription Plan : {plan}.")
-
-    else:
-        st.write("Username already exists.")
-        # raise typer.Abort()
-
+if "access_token" not in st.session_state or st.session_state['access_token']=='':
+    signup()
+else:
+    st.title("Please sign-out to access this feature!")
